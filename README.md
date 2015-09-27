f42ecbb1cb3ec7769e4b841c72e8ff143e2a3490
A1essandro <alexander.yermakov.1990@gmail.com>
2015-09-27 18:37:28 +0400

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # MapMaker
+
+##Example:
+```php
+
+use MapMaker\Base\Grid;
+use MapMaker\Base\Map;
+use MapMaker\DiamondAndSquare;
+use MapMaker\HtmlVisualiser;
+use MapMaker\WaterlineHtmlVisualiser;
+use MapMaker\WaterlineLayer;
+
+$map = new Map(new MapMaker\Base\Grid(75, 75));
+$map->setVisualiser(new MapMaker\HtmlVisualiser());
+
+$heightLayer = new MapMaker\DiamondAndSquare($map);
+$heightLayer->setMaxOffset(rand(100, 500));
+$heightLayer->generate();
+$map->attachLayer($heightLayer);
+
+$waterLayer = new MapMaker\WaterlineLayer($map);
+$waterLayer->setHeightLayerKey(\MapMaker\DiamondAndSquare::KEY);
+$waterLayer->setWaterRatio(1 / rand(4, 10));
+$waterLayer->generate();
+$map->attachLayer($waterLayer);
+
+$waterlineVisualiser = new \MapMaker\WaterlineHtmlVisualiser();
+$waterlineVisualiser->setLayer($waterLayer);
+$map->addLayerVisualiser($waterlineVisualiser);
+
+echo $map->render();
+
+```
--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 use MapMaker\Base\Grid;
 use MapMaker\Base\Map;
+use MapMaker\DiamondAndSquare;
+use MapMaker\HtmlVisualiser;
+use MapMaker\WaterlineHtmlVisualiser;
+use MapMaker\WaterlineLayer;
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
@@ -15,20 +19,20 @@ function __autoload($class)
 }
 
 $map = new Map(new Grid(75, 75));
-$map->setVisualiser(new MapMaker\HtmlVisualiser());
+$map->setVisualiser(new HtmlVisualiser());
 
-$heightLayer = new MapMaker\DiamondAndSquare($map);
+$heightLayer = new DiamondAndSquare($map);
 $heightLayer->setMaxOffset(rand(100, 500));
 $heightLayer->generate();
 $map->attachLayer($heightLayer);
 
-$waterLayer = new MapMaker\WaterlineLayer($map);
-$waterLayer->setHeightLayerKey(\MapMaker\DiamondAndSquare::KEY);
+$waterLayer = new WaterlineLayer($map);
+$waterLayer->setHeightLayerKey(DiamondAndSquare::KEY);
 $waterLayer->setWaterRatio(1 / rand(4, 10));
 $waterLayer->generate();
 $map->attachLayer($waterLayer);
 
-$waterlineVisualiser = new \MapMaker\WaterlineHtmlVisualiser();
+$waterlineVisualiser = new WaterlineHtmlVisualiser();
 $waterlineVisualiser->setLayer($waterLayer);
 $map->addLayerVisualiser($waterlineVisualiser);
 
