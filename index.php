5cdd5c40292ca00f0e163ca17c606ac647122cab
A1essandro <alexander.yermakov.1990@gmail.com>
2015-10-01 16:05:51 +0400

    Add new class HeightLayer, rendering without included Diamond&Square (with third-party package a1essandro/diamond-and-square)

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
+use MapGenerator\DiamondAndSquare;
 use MapMaker\Base\Grid;
 use MapMaker\Base\Map;
-use MapMaker\DiamondAndSquare;
+use MapMaker\HeightLayer;
 use MapMaker\HtmlVisualiser;
 use MapMaker\WaterlineHtmlVisualiser;
 use MapMaker\WaterlineLayer;
@@ -10,6 +11,8 @@ use MapMaker\WaterlineLayer;
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 header('Content-Type: text/html; charset=utf-8');
+
+require __DIR__ . '/vendor/autoload.php';
 
 function __autoload($class)
 {
@@ -21,10 +24,13 @@ function __autoload($class)
 $map = new Map(new Grid(75, 75));
 $map->setVisualiser(new HtmlVisualiser());
 
-$heightLayer = new DiamondAndSquare($map);
-$heightLayer->setMaxOffset(rand(100, 500));
+$ds = new DiamondAndSquare(7, 100);
+$ds->generate();
+$heightMap = $ds->getMap();
+
+$heightLayer = new HeightLayer($map, $heightMap);
+$heightLayer->cut(2, 2);
 $heightLayer->generate();
-$map->attachLayer($heightLayer);
 
 $waterLayer = new WaterlineLayer($map, $heightLayer);
 $waterLayer->setWaterRatio(1 / rand(2, 5));
---
+++ src/MapMaker/HeightLayer.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MapMaker;
+
+use Exception;
+use MapMaker\Base\Abstraction\IMap;
+use MapMaker\Base\Abstraction\Layer;
+
+class HeightLayer extends Layer
+{
+
+    private $unpreparedPoints;
+
+    /**
+     * @param IMap  $map
+     * @param float[][] $heightsArray 2d array
+     */
+    public function __construct(IMap $map, array $heightsArray)
+    {
+        parent::__construct($map);
+        if($this->checkInputArraySizes($heightsArray))
+            $this->unpreparedPoints = $heightsArray;
+    }
+
+    private function checkInputArraySizes($array)
+    {
+        $x = $this->map->getGrid()->getSizeX();
+        $y = $this->map->getGrid()->getSizeY();
+        if (empty($array[0][0]) || count($array) < $x || count($array[0]) < $y)
+            throw new Exception(printf('Heightmap sizes must be more than %dx%d', $x, $y));
+        return true;
+    }
+
+    /**
+     * Метод должен генерировать слой на основе заданных до этого параметров
+     */
+    public function generate()
+    {
+        if(count($this->unpreparedPoints) != $this->map->getGrid()->getSizeX()
+            || count($this->unpreparedPoints[0]) != $this->map->getGrid()->getSizeY())
+            $this->cut(0, 0);
+
+        foreach ($this->map->getGrid() as $coordinates) {
+            list($x, $y) = $coordinates;
+            $height = $this->unpreparedPoints[$x][$y];
+            $this->setCell($x, $y, new HeightCell($this, $height));
+        }
+    }
+
+    public function cut($startX, $startY)
+    {
+        $unpreparedX = count($this->unpreparedPoints);
+        $unpreparedY = count($this->unpreparedPoints[0]);
+
+        if($unpreparedX - $startX < $this->map->getGrid()->getSizeX() ||
+            $unpreparedY - $startY < $this->map->getGrid()->getSizeY())
+            throw new Exception("Small map");
+
+        $newUnpreparedPoints = array(array());
+        foreach ($this->map->getGrid() as $coordinates) {
+            list($ix, $iy) = $coordinates;
+            $newUnpreparedPoints[$ix][$iy] = $this->unpreparedPoints[$startX + $ix][$startY + $iy];
+        }
+
+        $this->unpreparedPoints = $newUnpreparedPoints;
+    }
+
+}
